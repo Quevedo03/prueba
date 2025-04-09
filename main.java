c66a88eee7040c58768b29d44cec20cd7427e5fa
Raul Quevedo <raulquevedo@192.168.1.17>
2025-04-08 19:27:59 -0700

    primera version

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+
+        
+        System.out.println("como estas");
+     
+    }
+
+    
+}
